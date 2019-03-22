34510a6096020e2110addc77b8105abf8008c537
LucasX <xulu0620@gmail.com>
2019-03-22 20:47:29 +0800

    update SQL script

--- xcloud.sql
+++ xcloud.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : XCloud
-Source Server Version : 50714
+Source Server         : hzau
+Source Server Version : 50715
 Source Host           : localhost:3306
 Source Database       : xcloud
 
 Target Server Type    : MYSQL
-Target Server Version : 50714
+Target Server Version : 50715
 File Encoding         : 65001
 
-Date: 2019-03-21 16:55:52
+Date: 2019-03-21 20:06:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -22,30 +22,40 @@ DROP TABLE IF EXISTS `api`;
 CREATE TABLE `api` (
   `username` varchar(16) NOT NULL,
   `api_name` varchar(20) NOT NULL,
-  `api_elapse` int(10) NOT NULL,
+  `api_elapse` float(4,0) NOT NULL,
   `api_call_datetime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `terminal_type` int(3) DEFAULT NULL
+  `terminal_type` int(3) DEFAULT NULL,
+  `img_path` varchar(100) DEFAULT NULL,
+  `skin_disease` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of api
 -- ----------------------------
+INSERT INTO `api` VALUES ('LucasX', 'cv/mcloud/skin', '0', '2019-03-21 17:45:05', '0', null, null);
+INSERT INTO `api` VALUES ('LucasX', 'cv/mcloud/skin', '0', '2019-02-06 17:45:34', '0', null, null);
+INSERT INTO `api` VALUES ('LucasX', 'cv/mcloud/skin', '0', '2019-03-14 17:45:58', '1', null, null);
+INSERT INTO `api` VALUES ('BigBear', 'cv/fbp', '0', '0000-00-00 00:00:00', '1', null, null);
+INSERT INTO `api` VALUES ('LucasX', 'cv/mcloud/skin', '0', '0000-00-00 00:00:00', '0', 'http://localhost:8000/static/SkinUpload/009899HB.jpg', 'Acne_Keloidalis_Nuchae\n');
+INSERT INTO `api` VALUES ('LucasX', 'cv/mcloud/skin', '1', '2019-03-21 20:02:31', '0', 'http://localhost:8000/static/SkinUpload/009895HB.jpg', 'Acne_Keloidalis_Nuchae\n');
 
 -- ----------------------------
--- Table structure for username
+-- Table structure for users
 -- ----------------------------
-DROP TABLE IF EXISTS `username`;
-CREATE TABLE `username` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   `username` varchar(16) NOT NULL,
   `register_datetime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
   `register_type` int(11) NOT NULL,
   `user_organization` varchar(100) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `key` varchar(20) NOT NULL,
+  `userkey` varchar(20) NOT NULL,
   `password` varchar(12) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of username
+-- Records of users
 -- ----------------------------
+INSERT INTO `users` VALUES ('LucasX', '2019-03-21 17:56:13', '0', 'Huazhong Agricultural University, College of Informatics', 'xulu0620@gmail.com', 'lucasx', '123456a');
+INSERT INTO `users` VALUES ('SmallBear', '0000-00-00 00:00:00', '0', 'Tsinghua University', 'smallbear@thu.edu.cn', 'smallbear', '66666a');
