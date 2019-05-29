a61bdaca34b118e0cfb3df16f00a205282ad1db5
LucasX <xulu0620@gmail.com>
2019-05-29 09:28:55 +0800

    update controller

--- cv/controllers.py
+++ cv/controllers.py
@@ -89,7 +89,7 @@ class BeautyRecognizer:
                           bbox[1] - margin_pixel: bbox[1] + bbox[3] + margin_pixel]
 
             cv2.rectangle(img, (bbox[0] - margin_pixel, bbox[1] - margin_pixel),
-                          (bbox[0] + bbox[2] + margin_pixel, bbox[1] + bbox[3] + margin_pixel), (0, 255, 0), 1)
+                          (bbox[0] + bbox[2] + margin_pixel, bbox[1] + bbox[3] + margin_pixel), (0, 255, 0), 2)
             cv2.imwrite(img_path, img)
 
             ratio = max(face_region.shape[0], face_region.shape[1]) / min(face_region.shape[0], face_region.shape[1])
