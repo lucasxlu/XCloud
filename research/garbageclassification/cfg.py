b51719d919fd9ccc9613699234c6457917daf9c7
LucasX <xulu0620@gmail.com>
2019-08-20 19:51:09 +0800

    Update cfg.py

--- research/garbageclassification/cfg.py
+++ research/garbageclassification/cfg.py
@@ -4,7 +4,7 @@ cfg = OrderedDict()
 
 cfg['garbage_classification_root'] = '/home/xulu/DataSet/garbage_classify'
 cfg['batch_size'] = 64
-cfg['epoch'] = 300
+cfg['epoch'] = 400
 cfg['init_lr'] = 0.01
 cfg['weight_decay'] = 1e-4
 cfg['lr_decay_step'] = 80
