87a0d70ab1c268738ab51338d6d734ff26f80867
LucasX <xulu0620@gmail.com>
2019-12-09 21:01:12 +0800

    add DeepPatchCNN

--- research/iqa/models.py
+++ research/iqa/models.py
@@ -95,6 +95,94 @@ class IQACNNPlusPlus(nn.Module):
 
         return num_features
 
+    
+class DeepPatchCNN(nn.Module):
+    """
+    A deep neural network for image quality assessment. ICIP'16
+    Sampling a patch with 32*32*3 from a given image and train a simple CNN for quality score regression
+    Input size: 32*32*3
+    """
+
+    def __init__(self, num_out):
+        super(DeepPatchCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)  # 32*32*32
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=1)  # 32*32*32
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU()
+        self.maxpool2 = nn.MaxPool2d(2, 2)  # 16*16*32
+
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)  # 16*16*64
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU()
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=1)  # 16*16*64
+        self.bn4 = nn.BatchNorm2d(64)
+        self.relu4 = nn.ReLU()
+        self.maxpool4 = nn.MaxPool2d(2, 2)  # 8*8*64
+
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=1)  # 8*8*128
+        self.bn5 = nn.BatchNorm2d(128)
+        self.relu5 = nn.ReLU()
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)  # 8*8*128
+        self.bn6 = nn.BatchNorm2d(128)
+        self.relu6 = nn.ReLU()
+        self.maxpool6 = nn.MaxPool2d(2, 2)  # 4*4*128
+
+        self.conv7 = nn.Conv2d(128, 256, 3, padding=1)  # 4*4*256
+        self.bn7 = nn.BatchNorm2d(256)
+        self.relu7 = nn.ReLU()
+        self.conv8 = nn.Conv2d(256, 256, 3, padding=1)  # 4*4*256
+        self.bn8 = nn.BatchNorm2d(256)
+        self.relu8 = nn.ReLU()
+        self.maxpool8 = nn.MaxPool2d(2, 2)  # 2*2*256
+
+        self.conv9 = nn.Conv2d(256, 512, 3, padding=1)  # 2*2*512
+        self.bn9 = nn.BatchNorm2d(512)
+        self.relu9 = nn.ReLU()
+        self.conv10 = nn.Conv2d(512, 512, 3, padding=1)  # 2*2*512
+        self.bn10 = nn.BatchNorm2d(512)
+        self.relu10 = nn.ReLU()
+        self.maxpool10 = nn.MaxPool2d(2, 2)  # 1*1*512
+
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(512, 512)
+        self.dropout1 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(512, num_out)
+
+    def forward(self, x):
+        x = self.relu1(self.bn1(self.conv1(x)))
+        x = self.maxpool2(self.relu2(self.bn2(self.conv2(x))))
+
+        x = self.relu3(self.bn3(self.conv3(x)))
+        x = self.maxpool4(self.relu4(self.bn4(self.conv4(x))))
+
+        x = self.relu5(self.bn5(self.conv5(x)))
+        x = self.maxpool6(self.relu6(self.bn6(self.conv6(x))))
+
+        x = self.relu7(self.bn7(self.conv7(x)))
+        x = self.maxpool8(self.relu8(self.bn8(self.conv8(x))))
+
+        x = self.relu9(self.bn9(self.conv9(x)))
+        x = self.maxpool10(self.relu10(self.bn10(self.conv10(x))))
+
+        x = x.view(-1, self.num_flat_features(x))
+
+        # If the size is a square you can only specify a single number
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = self.fc2(x)
+
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+
+        return num_features
+
 
 class NIMA(nn.Module):
     """
